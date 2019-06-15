5cffe609e487ab55bc262d583614880fdc9077af
Ep1c7MoDe <49326402+Ep1c7MoDe@users.noreply.github.com>
2019-06-15 07:09:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,3 +58,32 @@ client.on('message', async message => {
  
  
 client.login("NTg5MjYxMzIxMTI4OTAyNzE0.XQRGZQ.UPRg2a1RDF4Z2URA6UVZz3As1Jc");
+
+client.on("message", message => {
+    var prefix = "$";
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'bc') {
+        if (!args[1]) {
+    message.channel.send("**bc <message>**");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                m.send(args);
+            });
+            const AziRo = new Discord.RichEmbed()
+            .setAuthor(message.author.username, message.author.avatarURL)   
+            .setTitle('✅| جاري ارسال رسالتك ') 
+            .addBlankField(true)
+            .addField('♨| عدد الاعضاء المرسل لهم ', message.guild.memberCount , true)        
+            .addField('📝| الرسالة ', args)
+            .setColor('RANDOM')  
+            message.channel.sendEmbed(AziRo);          
+        }
+        } else {
+            return;
+        }
+    });
